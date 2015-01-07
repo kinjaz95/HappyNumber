fc80db35eb51ac7133ed8e73988b446ab4589def
Kevin Pham <kevin.pham@hotmail.de>
2015-01-07 16:04:23 +0100

    created unittest and implemented logic

---
+++ HappyNumber/HappyNumber.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HappyNumber
+{
+    public class HappyNumber
+    {
+        public int SplitNumber(int number)
+        {
+            var convertedNumber = number.ToString();
+            string[] convertedNumberList = convertedNumber.Split();
+
+            int summedNumber = 0;
+
+            foreach (string nr in convertedNumberList)
+            {
+                summedNumber += Int32.Parse(nr) ^ 2;
+            }
+
+            return summedNumber;
+        }
+
+        public bool IsNumberInList(int number, IList<int> numberList)
+        {
+            return numberList.Contains(number);
+        }
+
+        public bool IsCrossSumEqualsOne(int number)
+        {
+            var convertedNumber = number.ToString();
+            string[] convertedNumberList = convertedNumber.Split();
+
+            int summedNUmber = 0;
+
+            foreach (string nr in convertedNumberList)
+            {
+                summedNUmber += Int32.Parse(nr);
+            }
+
+            return summedNUmber == 1;
+        }
+
+    }
+}
---
+++ HappyNumberTest/HappyNumberTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace HappyNumberTest
+{
+    [TestClass]
+    public class HappyNumberTest
+    {
+        private HappyNumber _happyNumber;
+
+        public HappyNumberTest()
+        {
+            _happyNumber = new HappyNumber();
+        }
+        
+        [TestMethod]
+        public void SplitNumber_SplitNumberAndSquareAndSum_Number()
+        {
+            //Arrange: Given a number
+            int number = 52;
+
+            //Act: ... split number, square both numbers and sum them up ...
+            _happyNumber.SplitNumber(number);
+
+            //Assert: 
+            Assert.AreEqual(29, number);
+        }
+
+        [TestMethod]
+        public void ISNumberInList_ISNumberInList_Bool()
+        {
+            //Arrange:
+            int number = 52;
+            IList<int> DummyList = new List<int> { 20, 52, 13, 26, 15 };
+
+            //Act 
+            var isNumberInList = _happyNumber.IsNumberInList(number, DummyList);
+
+            //Assert
+            Assert.AreEqual(true, isNumberInList);
+        }
+
+        [TestMethod]
+
+        public void IsCrossSumEqualsOne_CheckIFCrossSumEqualsOne_bool()
+        {
+            //Arrange:
+            int number = 10;
+
+            //Act
+
+            var isCrossSumEqualsOne = _happyNumber.IsCrossSumEqualsOne(number);
+            //Assert
+            Assert.AreEqual(true, isCrossSumEqualsOne);
+        }
+    }
+
+
+}
